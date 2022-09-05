221034ec632bc8390854d410258d1c5bed45e7b3
DESKTOP-A4AKDVO\Pc <barsmano@gmail.com>
2022-09-05 03:33:37 +0300

    +added store "Todos" in localstorage +added trashbtn to remove localstorage todos

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const todoInput =document.querySelector('.todo-input');
 const todoButton =document.querySelector('.todo-button');
 const todoList =document.querySelector('.todo-list');
-const Body = document.querySelector('.body');
+// const Body = document.querySelector('.body');
+const filterOption = document.querySelector('.filter-todo');
 
 
 
+document.addEventListener('DOMContentLoaded',getTodos);
 
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click',deleteCheck);
+filterOption.addEventListener('change',filterTodo);
+
+// if(localStorage.getItem('todos')==[]){
+//   // const notodotext =document.querySelector('.not_todo_text');
+//   // notodotext.display='flex';
+//   document.getElementById("no-todo-text").style.visibility = "hidden";
+// }
 
 
 function addTodo(event){
@@ -23,6 +32,9 @@ function addTodo(event){
     newTodo.innerText=todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
+
+   
+
     //check mark button
     const completedButton =document.createElement('button');
     completedButton.innerHTML='<i class="fas fa-check"></i>';
@@ -34,8 +46,10 @@ function addTodo(event){
     trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
 
+
     //append List
     if(todoInput.value!=""){
+      saveLocalTodos(todoInput.value);
         todoList.appendChild(todoDiv);
         todoInput.value="";
     }
@@ -55,132 +69,118 @@ function deleteCheck(e){
  const item = e.target;
   if(item.classList[0]==='trash-btn'){
     const todo = item.parentElement;
-    todo.remove();
+    // console.log(todo.classList);
+    todo.classList.add('fall');
+
+    removeLocalTodo(todo);
+
+    todo.addEventListener('transitionend',function(){
+      todo.remove();
+    });
   }
   if(item.classList[0]==='completed-btn'){
     const todo= item.parentElement;
     todo.classList.toggle("completed");
+    
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) { 
+      const mStyle = todo.style;  
+      if(mStyle != undefined && mStyle != null){
+          switch (e.target.value) {
+              case "all":
+                  mStyle.display = "flex";
+                  break;
+              case "completed":
+                  if (todo.classList.contains('completed')) {
+                      mStyle.display = 'flex';
+                  } else {
+                      mStyle.display = "none";
+                  }
+                  break;
+              case "uncompleted":
+                  if (todo.classList.contains('completed')){
+                      mStyle.display = 'none';
+                  }
+                  else{
+                      mStyle.display = "flex";
+                  }
+                  break;
+          }
+      }
+  });
 }
 
 
 
-// var shakingElements = [];
+function saveLocalTodos(todo){
+  let todos;
+  if(localStorage.getItem('todos')===null){
+    todos =[];
 
-// var shake = function (element, magnitude = 16, angular = false) {
-//   //First set the initial tilt angle to the right (+1) 
-//   var tiltAngle = 1;
+  }
+  else{
+    todos =JSON.parse(localStorage.getItem('todos'));
+  }
 
-//   //A counter to count the number of shakes
-//   var counter = 1;
+  todos.push(todo);
+  localStorage.setItem("todos",JSON.stringify(todos));
+}
 
-//   //The total number of shakes (there will be 1 shake per frame)
-//   var numberOfShakes = 15;
+function getTodos(){
+  let todos;
+  if(localStorage.getItem("todos")===null){
+    todos=[];
+  }else{
+    todos=JSON.parse(localStorage.getItem("todos"));
+  }
 
-//   //Capture the element's position and angle so you can
-//   //restore them after the shaking has finished
-//   var startX = 0,
-//       startY = 0,
-//       startAngle = 0;
+  
 
-//   // Divide the magnitude into 10 units so that you can 
-//   // reduce the amount of shake by 10 percent each frame
-//   var magnitudeUnit = magnitude / numberOfShakes;
+  todos.forEach(function(todo){
 
-//   //The `randomInt` helper function
-//   var randomInt = (min, max) => {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-//   };
+      //Todo Div
+      const todoDiv = document.createElement("div");
+      todoDiv.classList.add("todo");
+      //li create
+      const newTodo = document.createElement("li");
+      newTodo.innerText=todo;
+      newTodo.classList.add("todo-item");
+      todoDiv.appendChild(newTodo);
+      //check mark button
+      const completedButton =document.createElement('button');
+      completedButton.innerHTML='<i class="fas fa-check"></i>';
+      completedButton.classList.add("completed-btn");
+      todoDiv.appendChild(completedButton);
+      //check trash button
+      const trashButton =document.createElement('button');
+      trashButton.innerHTML='<i class="fas fa-trash"></i>';
+      trashButton.classList.add("trash-btn");
+      todoDiv.appendChild(trashButton);
 
-//   //Add the element to the `shakingElements` array if it
-//   //isn't already there
-//   if(shakingElements.indexOf(element) === -1) {
-//     //console.log("added")
-//     shakingElements.push(element);
+      todoList.appendChild(todoDiv);
+      
 
-//     //Add an `updateShake` method to the element.
-//     //The `updateShake` method will be called each frame
-//     //in the game loop. The shake effect type can be either
-//     //up and down (x/y shaking) or angular (rotational shaking).
-//     if(angular) {
-//       angularShake();
-//     } else {
-//       upAndDownShake();
-//     }
-//   }
+      
+    });
 
-//   //The `upAndDownShake` function
-//   function upAndDownShake() {
+}
 
-//     //Shake the element while the `counter` is less than 
-//     //the `numberOfShakes`
-//     if (counter < numberOfShakes) {
+function removeLocalTodo(todo){
+  let todos;
+  if(localStorage.getItem("todos")===null){
+    todos=[];
+  }
+  else{
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex),1);
+  localStorage.setItem("todos",JSON.stringify(todos));
+}
 
-//       //Reset the element's position at the start of each shake
-//       element.style.transform = 'translate(' + startX + 'px, ' + startY + 'px)';
 
-//       //Reduce the magnitude
-//       magnitude -= magnitudeUnit;
 
-//       //Randomly change the element's position
-//       var randomX = randomInt(-magnitude, magnitude);
-//       var randomY = randomInt(-magnitude, magnitude);
-
-//       element.style.transform = 'translate(' + randomX + 'px, ' + randomY + 'px)';
-
-//       //Add 1 to the counter
-//       counter += 1;
-
-//       requestAnimationFrame(upAndDownShake);
-//     }
-
-//     //When the shaking is finished, restore the element to its original 
-//     //position and remove it from the `shakingElements` array
-//     if (counter >= numberOfShakes) {
-//       element.style.transform = 'translate(' + startX + ', ' + startY + ')';
-//       shakingElements.splice(shakingElements.indexOf(element), 1);
-//     }
-//   }
-
-//   //The `angularShake` function
-//   function angularShake() {
-//     if (counter < numberOfShakes) {
-//       console.log(tiltAngle);
-//       //Reset the element's rotation
-//       element.style.transform = 'rotate(' + startAngle + 'deg)';
-
-//       //Reduce the magnitude
-//       magnitude -= magnitudeUnit;
-
-//       //Rotate the element left or right, depending on the direction,
-//       //by an amount in radians that matches the magnitude
-//       var angle = Number(magnitude * tiltAngle).toFixed(2);
-//       console.log(angle);
-//       element.style.transform = 'rotate(' + angle + 'deg)';
-//       counter += 1;
-
-//       //Reverse the tilt angle so that the element is tilted
-//       //in the opposite direction for the next shake
-//       tiltAngle *= -1;
-
-//       requestAnimationFrame(angularShake);
-//     }
-
-//     //When the shaking is finished, reset the element's angle and
-//     //remove it from the `shakingElements` array
-//     if (counter >= numberOfShakes) {
-//       element.style.transform = 'rotate(' + startAngle + 'deg)';
-//       shakingElements.splice(shakingElements.indexOf(element), 1);
-//       //console.log("removed")
-//     }
-//   }
-
-// };
-
-// document.querySelector('.todo-input').addEventListener('mouseenter', (e) => {
-//   shake(e.currentTarget);
-// });
-
-// document.querySelector('.green').addEventListener('mouseenter', (e) => {
-//   shake(e.currentTarget, 20, true);
-// });
